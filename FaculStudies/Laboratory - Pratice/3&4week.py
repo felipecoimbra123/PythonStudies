d1e439dbaa1393085dae1b36f5acd6988f2fc568
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2026-05-18 19:21:31 -0300

    exercises 'for' solved

--- FaculStudies/Laboratory - Pratice/3&4week.py
+++ FaculStudies/Laboratory - Pratice/3&4week.py
@@ -235,54 +235,49 @@ print("Números digitados:", numeros)
 # Exercício 1. Crie um programa imprime na tela os valores de 1 a 100 (incluindo o 1 e o
 # 100).
 
+for numeros in range(1, 101):
+    print(numeros)
+
+
 # Exercício 2. Crie um programa que imprime na tela todos os valores entre dois valores
 # digitados pelo teclado.
+
+inicio = int(input("Digite o valor inicial: "))
+fim = int(input("Digite o valor final: "))
+
+for i in range(inicio, fim + 1):
+    print(i)
+
 
 # Exercício 3. Crie um programa que imprime a tabuada de um número qualquer digitado
 # pelo usuário.
 
+numero = int(input("Digite o seu número: "))
+
+for i in range(1, 11):
+    print(numero, "*", i, "=", numero * i)
+
 # Exercício 4. Sabendo que uma string é uma lista de letras, peça para o usuário digitar
 # um texto e imprima na tela a quantidade de vogais que existem no texto.
+
+letra = input("Digite o texto: ")
+vogais = 0
+
+for i in letra:
+    if(i == "A" or i == "a" or i == "E" or i == "e" or i == "I" or i == "i" or i == "O" or i == "o" or i == "U" or i == "u"):
+        vogais += 1
+print(vogais)
 
 # Exercício 5. Crie um programa que permita que o usuário crie sua lista de compras.
 # Primeiramente, solicite que ele informe quantos produtos serão adicionados na lista.
 # Depois disto, peça para que o usuário digite os produtos que ele vai comprar, e
 # armazene em uma lista. Ao final, imprima a lista de compras do usuário.
 
-# Exercício 6. Crie um programa que solicita o nome e o estado civil de 20 pessoas pelo
-# teclado. Ao final, imprima apenas o nome das pessoas separadas por estado civil:
-# solteiras, casadas, divorciadas e viúvas (nesta ordem!)
+quant = int(input("Quantos produtos você terá na lista? "))
+listaDeCompras = []
 
-# Exercício 7. Crie um programa que solicita ao usuário que ele defina sua senha. A senha
-# deve ser um texto (string) composto apenas por dígitos e deve ter entre 5 e 10 valores.
-# O usuário tem apenas 6 chances de definir corretamente a senha. Caso ele defina
-# corretamente a senha nas tentativas que ele tem, imprima uma mensagem de sucesso.
-# Caso ele não defina a senha corretamente, imprima uma mensagem de insucesso. Dica:
-# na aula aprendemos a ver se uma string é formada apenas por dígitos e aprendemos a
-# descobrir o tamanho do texto digitado.
-
-# Exercício 8. Crie um programa que separa o joio do trigo. Seu programa deve ler a lista
-# abaixo e criar duas listas diferentes: uma com todas as ocorrências da palavra "joio" e
-# outra com todas as ocorrências da palavra "trigo". Ao final, imprima as listas separadas.
-# Copie e cole a linha abaixo no seu código e complete o programa:
-# joioETrigo = ["joio", "trigo", "trigo", "joio", "trigo",
-# "joio", "joio", "joio", "joio", "trigo", "trigo", "joio",
-# "joio", "joio", "trigo", "trigo", "trigo", "trigo", "trigo",
-# "trigo", "trigo", "trigo", "trigo", "trigo", "trigo",
-# "joio", "joio", "joio", "joio", "joio", "joio", "joio",
-# "joio", "trigo", "trigo", "joio", "joio", "joio", "joio",
-# "joio", "joio", "joio", "joio", "joio", "joio", "joio",
-# "joio", "joio", "joio", "joio", "joio", "trigo", "trigo",
-# "trigo", "trigo", "trigo", "trigo", "trigo", "trigo",
-# "trigo", "trigo", "trigo", "trigo", "trigo", "trigo",
-# "trigo", "trigo", "trigo", "trigo", "joio", "joio", "joio",
-# "joio", "joio", "joio", "joio", "joio", "joio", "joio",
-# "trigo", "trigo", "trigo", "trigo", "trigo", "trigo",
-# "trigo", "trigo", "trigo", "joio", "joio", "joio", "joio",
-# "joio", "joio", "trigo", "joio", "joio", "joio", "joio",
-# "joio", "trigo", "trigo", "trigo", "trigo", "joio", "joio",
-# "joio", "joio", "joio", "joio", "joio", "trigo", "trigo",
-# "trigo", "joio", "trigo", "joio", "joio", "joio"]
-
-# Exercício 9. Faça novamente todos os exercícios das listas de exercícios sobre WHILE,
-# porém utilizando o for para realizar a repetição.
+for i in range(0,quant):
+    listaDeCompras.append(input("Digite o produto:"))
+print("Lista de compras:")
+for i in listaDeCompras:
+    print(i)
